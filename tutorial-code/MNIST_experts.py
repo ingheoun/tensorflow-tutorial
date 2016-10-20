9fdf2d6341d747865b1e0dcbeb5d66336786456e
ingheoun <ingheoun@kaist.ac.kr>
2016-10-20 22:53:53 +0900

    Update MNIST_experts.py

--- tutorial-code/MNIST_experts.py
+++ tutorial-code/MNIST_experts.py
@@ -6,34 +6,79 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 import tensorflow as tf
 sess = tf.InteractiveSession()
 
-# Make a placeholder for training batch
-x = tf.placeholder(tf.float32, shape=[None, 784])
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
+# Make function which returns initialized weight (range (0,1)) and bias (0.1) variable tensor.
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
 
-# Make W (weight) and b(bias) for regression 
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
 
-# Initialize the variables
-sess.run(tf.initialize_all_variables())
+# Make function for 2d-convolution and max pooling
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-# Calculate the output of regression and softmax
-y = tf.nn.softmax(tf.matmul(x,W) + b)
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+# reshape input image tensor
+x_image = tf.reshape(x, [-1,28,28,1])
+
+# 1st conv layer init
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+#output of 1st layer
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+# 2nd conv layer
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+# 3rd FC layer
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+# Make prob variable for dropout setting
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+# 4rd FC layer and softmax
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 # Design a cost function model with cross-entropy
 # reduction_indices = [1] means sum through the dimension 1 (col-dimension)
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y), reduction_indices = [1]))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+
 
 ## Training phase
-# Use gradient descent w/ lr = 0.5
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+# Use Adam optimizer
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-# assign batch by feed_dict
-for i in range(1000):
-  batch = mnist.train.next_batch(50)
-  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-
-# calculate accuracy
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+# calculate accuracy var
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+## Session run
+sess.run(tf.initialize_all_variables())
+for i in range(20000):
+  batch = mnist.train.next_batch(50)
+  if i%100 == 0:
+    train_accuracy = accuracy.eval(feed_dict={
+        x:batch[0], y_: batch[1], keep_prob: 1.0})
+    print("step %d, training accuracy %g"%(i, train_accuracy))
+  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+print("test accuracy %g"%accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
